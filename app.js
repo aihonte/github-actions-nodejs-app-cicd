12b9ee53277253d538462bf3ae8b845fe7870f48
aihonte <c01040593597@gmail.com>
2025-11-29 04:22:58 +0900

    update app.js file

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`The best Node JS Server is  running at http://${hostname}:${port}/`);
+  console.log(`Hot Node JS Server is  running at http://${hostname}:${port}/`);
 });
 
 module.exports = server;
